2187906755a43be1ee6a1a93579ffb79deac1538
Jon Samos <jonsamos@uw.edu>
2017-01-10 15:21:56 -0800

    Class work1/10

---
+++ exercise-1/exercise-1 module 5.R
@@ -0,0 +1,25 @@
+# Exercise-1: practice with basic syntax
+
+# Create a variable `hometown` that stores the city in which you were born
+hometown <- "Palo Alto"
+
+# Assign your name to the variable `my.name`
+my.name <- "Jon Samos"
+
+# Assign your height (in inches) to a variable `my.height`
+my.height <- "74"
+
+# Create a variable `puppies` equal to the number of puppies you'd like to have
+puppies <- 1
+
+# Create a variable `puppy.price`, which is how expensive you think a puppy is
+puppy.price <- 500
+
+# Create a variable `total.cost` that has the total cost of all of your puppies
+total.cost <- 500
+
+# Create a boolean variable `too.expensive`, set to TRUE if the cost is greater than $1,000
+too.expensive <- FALSE
+
+# Create a variable `max.puppies`, which is the number of puppies you can afford for $1,000.
+max.puppies <- 2
